f6ccb4a66f5ec798ab06be82da4a06a3fb95e577
puppy09 <zepedaramireztics@gmail.com>
2024-11-02 01:13:08 -0700

    login css fixed

--- sweeney/src/app/app.routes.ts
+++ sweeney/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {path:'', component: LoginComponent},
-    {path: 'menu', component: MenuPrincipalComponent}
+    {path: 'menu', component: MenuPrincipalComponent},
+    {path: 'registro',component:RegisterComponent}
 ];

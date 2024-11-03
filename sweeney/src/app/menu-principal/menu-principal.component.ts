22d058334456cf869493e4812c11cc0c9f511d9a
puppy09 <zepedaramireztics@gmail.com>
2024-11-03 14:47:50 -0700

    Servicio cuenta cureado pero sin nada

--- sweeney/src/app/menu-principal/menu-principal.component.ts
+++ sweeney/src/app/menu-principal/menu-principal.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { SidebarComponent } from "../sidebar/sidebar.component";
 @Component({
   selector: 'app-menu-principal',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './menu-principal.component.html',
   styleUrl: './menu-principal.component.css'
 })
--- sweeney/src/app/sidebar/sidebar.component.ts
+++ sweeney/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SidebarModule, ButtonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  sidebarVisible: boolean = true;
 }

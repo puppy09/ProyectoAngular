0e011c6e4ffb8366498071d423efe0bdca2c05a2
puppy09 <zepedaramireztics@gmail.com>
2024-11-04 23:41:35 -0700

    Servicio Get cuentas

--- sweeney/src/app/services/cuentas/cuentas.service.ts
+++ sweeney/src/app/services/cuentas/cuentas.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environtment';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuentasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private apiUrl = `${environment.apiUrl}/cuentas`;
+
+  getCuentas():Observable<any>{
+    return this.http.get<any>(this.apiUrl);
+  }
 }

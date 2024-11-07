f84fc079005053201c6cacf172690932b1e21148
puppy09 <zepedaramireztics@gmail.com>
2024-11-07 02:34:12 -0700

    Servicio get de grupos creados

---
+++ sweeney/src/app/interfaces/gruposCreados.interface.ts
@@ -0,0 +1,8 @@
+export interface gruposCreados {
+    id_grupo: number;
+    nombre: string;
+    descripcion: string;
+    id_creador: number;
+    fondos: number;
+    token: string;
+}
--- sweeney/src/app/services/grupos/grupos-creados.service.ts
+++ sweeney/src/app/services/grupos/grupos-creados.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environtment';
+import { HttpClient } from '@angular/common/http';
+import { gruposCreados } from '../../interfaces/gruposCreados.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GruposCreadosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private apiUrl = `${environment.apiUrl}/grupos/creados`;
+
+  getGruposCreados(): Observable<gruposCreados|gruposCreados[]>{
+    return this.http.get<gruposCreados|gruposCreados[]>(this.apiUrl);
+  }
 }

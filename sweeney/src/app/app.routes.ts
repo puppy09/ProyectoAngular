d745380649f6083537f4f2bfe109110ce06a0b84
puppy09 <zepedaramireztics@gmail.com>
2024-11-09 02:21:00 -0700

    sidebar funcional

--- sweeney/src/app/app.routes.ts
+++ sweeney/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { LoginComponent } from './components/login/login.component';
 import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CuentasComponent } from './components/cuentas/cuentas.component';
+import { MovimientosComponent } from './components/movimientos/movimientos.component';
+import { GruposComponent } from './components/grupos/grupos.component';
+import { GastosComponent } from './components/gastos/gastos.component';
 
 export const routes: Routes = [
     {path:'', component: LoginComponent},
     {path: 'menu', component: MenuPrincipalComponent},
     {path: 'registro',component:RegisterComponent},
-    {path: 'cuentas', component: CuentasComponent}
+    {path: 'cuentas', component: CuentasComponent},
+    {path: 'movimientos',component:MovimientosComponent},
+    {path: 'grupos',component:GruposComponent},
+    {path: 'gastos',component:GastosComponent}
 ];
--- sweeney/src/app/components/sidebar/sidebar.component.ts
+++ sweeney/src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import {ButtonModule} from "primeng/button";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,4 +12,22 @@ import {ButtonModule} from "primeng/button";
 })
 export class SidebarComponent {
   sidebarVisible: boolean = true;
+  constructor(private router:Router){}
+  
+  goToMovimientos(){
+    this.router.navigate(['/movimientos']);
+  }
+
+  goToCuentas(){
+    this.router.navigate(['/cuentas']);
+  }
+
+  goToGrupos(){
+    this.router.navigate(['/grupos']);
+  }
+
+  goToGastos(){
+    this.router.navigate(['/gastos']);
+  }
 }
+

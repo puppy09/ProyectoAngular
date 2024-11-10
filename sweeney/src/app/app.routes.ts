58a56ea6fb2c7765f3d263609d231ee9c5661d83
puppy09 <zepedaramireztics@gmail.com>
2024-11-09 23:48:10 -0700

    Implementación de pagina 404

--- sweeney/src/app/app.routes.ts
+++ sweeney/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CuentasComponent } from './components/cuentas/cuentas.component';
 import { MovimientosComponent } from './components/movimientos/movimientos.component';
 import { GruposComponent } from './components/grupos/grupos.component';
 import { GastosComponent } from './components/gastos/gastos.component';
+import { Page404Component } from './components/page404/page404.component';
 
 export const routes: Routes = [
     {path:'', component: LoginComponent},
@@ -14,5 +15,9 @@ export const routes: Routes = [
     {path: 'cuentas', component: CuentasComponent},
     {path: 'movimientos',component:MovimientosComponent},
     {path: 'grupos',component:GruposComponent},
-    {path: 'gastos',component:GastosComponent}
+    {path: 'gastos',component:GastosComponent},
+    {path: 'page404', component:Page404Component},
+    {
+        path: '**', redirectTo: 'page404'
+    }
 ];

481a002db64403ddf65bedeae8c4d99cf34b52c1
puppy09 <zepedaramireztics@gmail.com>
2024-11-11 01:46:30 -0700

    base de formulario reactivo de post de pagos

--- sweeney/src/app/components/movimientos-form/movimientos-form.component.ts
+++ sweeney/src/app/components/movimientos-form/movimientos-form.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-movimientos-form',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [HeaderComponent, SidebarComponent, ReactiveFormsModule],
   templateUrl: './movimientos-form.component.html',
   styleUrl: './movimientos-form.component.css'
 })
 export class MovimientosFormComponent {
 
+  formlarioNewPago = new FormGroup({
+    descripcion: new FormControl(''),
+
+  })
 }

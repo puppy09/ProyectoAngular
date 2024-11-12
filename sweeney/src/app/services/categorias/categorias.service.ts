86ad0324f1a8bfe30ee91bc5551a019fdd416321
puppy09 <zepedaramireztics@gmail.com>
2024-11-12 00:15:02 -0700

    servicio get de categorias creado

--- sweeney/src/app/services/categorias/categorias.service.ts
+++ sweeney/src/app/services/categorias/categorias.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environtment';
+import { Observable } from 'rxjs';
+import { categoria } from '../../interfaces/categorias.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  private apiUrl = `${environment.apiUrl}/category`;
+
+  getCategorias():Observable<categoria|categoria[]>{
+    return this.http.get<categoria|categoria[]>(this.apiUrl);
+  }
 }

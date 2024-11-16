f57de1641c315e1b18a6e8db8089e4c75d0eeb1b
puppy09 <zepedaramireztics@gmail.com>
2024-11-16 13:49:24 -0700

    Pagos editables

--- sweeney/src/app/components/pagosUpdateForm/pagos-update-form/pagos-update-form.component.ts
+++ sweeney/src/app/components/pagosUpdateForm/pagos-update-form/pagos-update-form.component.ts
@@ -7,6 +7,8 @@ import { SubcategoriasService } from '../../../services/subcategorias/subcategor
 import { DataServiceService } from '../../../services/dataService/data-service.service';
 import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { HeaderComponent } from '../../header/header.component';
+import { PagosService } from '../../../services/pagos/pagos.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-pagos-update-form',
   standalone: true,
@@ -20,8 +22,8 @@ export class PagosUpdateFormComponent {
   cuentas: any;
   categorias: any;
   subcategorias: any;
-  
-  constructor(private dataSvc: DataServiceService,  private fb: FormBuilder, private route: Router, private cuenSrv: CuentasService, private  catSrv: CategoriasService, private subSrv: SubcategoriasService){
+  pago: any;
+  constructor(private snackBar: MatSnackBar, private pagoSvc: PagosService,  private dataSvc: DataServiceService,  private fb: FormBuilder, private route: Router, private cuenSrv: CuentasService, private  catSrv: CategoriasService, private subSrv: SubcategoriasService){
     this.pagosForm = this.fb.group({
       no_cuenta: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -33,15 +35,15 @@ export class PagosUpdateFormComponent {
       totalPagos: new FormControl({value:'', disabled: true})
     })
     
-    const pago = this.dataSvc.getPagoData();
-    if(pago){
-      console.log(pago);
+    this.pago = this.dataSvc.getPagoData();
+    if(this.pago){
+      console.log(this.pago);
       this.pagosForm.patchValue({
-        no_cuenta: pago.no_cuenta,
-        categoria: pago.categoria,
-        subcategoria: pago.subcategoria,
-        descripcion: pago.descripcion,
-        monto: pago.monto
+        no_cuenta: this.pago.no_cuenta,
+        categoria: this.pago.categoria,
+        subcategoria: this.pago.subcategoria,
+        descripcion: this.pago.descripcion,
+        monto: this.pago.monto
       });
     }else{
       console.log("No hay pago");
@@ -87,5 +89,29 @@ export class PagosUpdateFormComponent {
         console.error('Error fetching subcategorias: ', error);
       }
     )
+  }
+
+  submitForm(){
+    //if(this.pagosForm.valid){
+      const formData=this.pagosForm.value;
+      this.pagoSvc.updPago(
+        this.pago.id_pago,
+        formData.no_cuenta,
+        formData.descripcion,
+        formData.monto,
+        formData.categoria,
+        formData.subcategoria
+      ).subscribe(response=>{
+        this.snackBar.open('Pago Actualizado con exito', 'Cerrar',{
+            duration: 5000
+        })}, error=>{
+          const errorMessage = error.error?.message || 'Error al actualizar pago';
+            this.snackBar.open('Error actualizando pago'+errorMessage, 'Cerrar',{
+            duration: 5000
+        })}
+    )//}
+    //else{
+      //alert("Formulario Invalido o fondos insuficientes");
+    //}
   }
 }

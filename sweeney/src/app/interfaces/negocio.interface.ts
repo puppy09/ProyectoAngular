32b3537f4e9324e5a7232d363c6abd62b94b2557
puppy09 <zepedaramireztics@gmail.com>
2024-11-22 15:48:09 -0700

    interface de negocio creada

---
+++ sweeney/src/app/interfaces/negocio.interface.ts
@@ -0,0 +1,8 @@
+export interface negocio{
+    id_negocio:number,
+    nombre:string,
+    tipo_negocio:number,
+    negocioRubro:{
+        tipo_negocio:string
+    }
+}
--- sweeney/src/app/services/negocios/negocios.service.ts
+++ sweeney/src/app/services/negocios/negocios.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environtment';
+import { Observable } from 'rxjs';
+import { negocio } from '../../interfaces/negocio.interface';
+import { negocioRubro } from '../../interfaces/negocioRubro.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NegociosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private apiUrl = `${environment.apiUrl}/negocios`;
+
+  getRubros():Observable
 }

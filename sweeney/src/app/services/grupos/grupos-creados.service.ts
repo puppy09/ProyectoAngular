086e6e8b889fcef2509cce7bcbe1ae5c7566dc69
puppy09 <zepedaramireztics@gmail.com>
2024-11-24 19:20:47 -0700

    servicio de get de movimientos grupales creado

--- sweeney/src/app/services/grupos/grupos-creados.service.ts
+++ sweeney/src/app/services/grupos/grupos-creados.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environtment';
 import { HttpClient } from '@angular/common/http';
 import { gruposCreados, gruposMiembro } from '../../interfaces/gruposCreados.interface';
-import { gruposPagos } from '../../interfaces/gruposPagos.interface';
+import { gruposMovimientos, gruposPagos } from '../../interfaces/gruposPagos.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,6 +13,7 @@ export class GruposCreadosService {
   constructor(private http: HttpClient) { }
   private apiUrl = `${environment.apiUrl}/grupos`;
   private apiURL2 = `${environment.apiUrl}/pagosGrupos`;
+  private apiURL3 = `${environment.apiUrl}/movimientos_grupales`;
 
   getGruposCreados(): Observable<gruposCreados|gruposCreados[]>{
     return this.http.get<gruposCreados|gruposCreados[]>(`${this.apiUrl}/creados`);
@@ -32,5 +33,9 @@ export class GruposCreadosService {
 
   getPagosGrupales(id_grupo:string): Observable<gruposPagos|gruposPagos[]>{
     return this.http.get<gruposPagos|gruposPagos[]>(`${this.apiURL2}/${id_grupo}`);
+  }
+
+  getMovimientosGrupales(id_grupo:string):Observable<gruposMovimientos|gruposMovimientos[]>{
+    return this.http.get<gruposMovimientos|gruposMovimientos[]>(`${this.apiURL3}/${id_grupo}`);
   }
 }
